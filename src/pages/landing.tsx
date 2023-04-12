aad323b3345e2facab79dab5636ea8a544df88dd
johnma <ma.jonathan02@gmail.com>
2023-04-11 20:07:13 -0400

    reverted to using HashRouter, finished prototype for Cards

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // Chakra
-import { extendTheme, ChakraProvider, Box } from "@chakra-ui/react";
+import { extendTheme, ChakraProvider, Box, Text} from "@chakra-ui/react";
 import {Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { Heading, Grid } from "@chakra-ui/react";
@@ -22,7 +22,14 @@ const colors = {
         700: "#2a69ac",
     },
 };
+const presetCards =  [
+    "red.400", "orange", "green", "teal", "blue",
+    "cyan", "purple.400", "purple.800", "pink", "white"];
 
+const themeProps = [
+    "Accent", "Background", "Foreground", "UI", "Function",
+    "String", "Operator", "Comment", "Error"
+];
 const theme = extendTheme({ colors });
 
 //Routing
@@ -32,105 +39,123 @@ import {Link} from "react-router-dom";
 export default function Landing() {
     return (
         <ChakraProvider theme={theme}>
-            <Heading>Neovim Theme Generator</Heading>
-            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-                <div>
-                    <Tabs>
-                        <TabList>
-                            <Tab>Presets</Tab>
-                            <Tab>Color Wheel</Tab>
-                            <Tab>Hexcode</Tab>
-                        </TabList>
+            <div style={{padding: 40}}>
+                <Heading height="75px">Neovim Theme Generator</Heading>
+                <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+                    <div>
+                        <Text height="40px">Drag and drop colors to get started!</Text>
+                        <Tabs>
+                            <TabList>
+                                <Tab>Presets</Tab>
+                                <Tab>Color Wheel</Tab>
+                                <Tab>Hexcode</Tab>
+                            </TabList>
 
-                        <TabPanels>
-                            <TabPanel>
+                            <TabPanels>
+                                <TabPanel>
 
-                                <Card>
-                                    <CardBody>
-                                      Col 1
-                                    </CardBody>
-                                </Card>
-                            </TabPanel>
-                            <TabPanel>
-                                <Card>
-                                    <CardBody>
+                                    <Card>
+                                        <CardBody>
+                                            <Grid templateColumns="repeat(5,1fr)" gap={2}>
+                                                {presetCards.map((x: string)=>(
+                                                    <Card key={x} backgroundColor={x} variant="filled" size="lg">
+                                                        <CardBody></CardBody>
+                                                    </Card>
+                                                ))}
+                                            </Grid>
+                                        </CardBody>
+                                    </Card>
+                                </TabPanel>
+                                <TabPanel>
+                                    <Card>
+                                        <CardBody>
                                       Col 2
-                                    </CardBody>
-                                </Card>
-                            </TabPanel>
-                            <TabPanel>
-                                <Card>
-                                    <CardBody>
+                                        </CardBody>
+                                    </Card>
+                                </TabPanel>
+                                <TabPanel>
+                                    <Card>
+                                        <CardBody>
                                       Col 3
-                                    </CardBody>
-                                </Card>
-                            </TabPanel>
-                        </TabPanels>
-                    </Tabs>
-                    <Card>
-                        <CardBody>
-                        Drag and Drops
-                        </CardBody>
-                    </Card>
-                </div>
-                <div>
-                    <b>Preview</b>
-                    <Image boxSize="lg" src="https://via.placeholder.com/500x300/0077be/ffffff?text=Placeholder+Image"></Image>
-                    <Button colorScheme="blue">Download .lua File</Button>
-                </div>
-                <div>
-                    <Button colorScheme="blue">
-                        <Link to="edit">Edit Presets</Link>
-                    </Button>
-                    <Accordion>
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box as="span" flex='1' textAlign='left'>
-                                      Presets
-                                    </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel pb={4}>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                              commodo consequat.
-                            </AccordionPanel>
-                        </AccordionItem>
-
-                        <AccordionItem>
-                            <h2>
-                                <AccordionButton>
-                                    <Box as="span" flex='1' textAlign='left'>
-                                      Saved Themes
-                                    </Box>
-                                    <AccordionIcon />
-                                </AccordionButton>
-                            </h2>
-                            <AccordionPanel pb={4}>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                              commodo consequat.
-                            </AccordionPanel>
-                        </AccordionItem>
-                    </Accordion>
-                    <div style={{bottom: 0, position: "absolute"}}>
-                        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-                            <Select placeholder='Select option'>
-                                <option value='option1'>Option 1</option>
-                                <option value='option2'>Option 2</option>
-                                <option value='option3'>Option 3</option>
-                            </Select>
-                            <Button colorScheme="blue" >
-                                <Link to="users">Edit Users</Link>
-                            </Button>
-                        </Grid>
+                                        </CardBody>
+                                    </Card>
+                                </TabPanel>
+                            </TabPanels>
+                        </Tabs>
+                        <Card>
+                            <CardBody>
+                                <Grid templateColumns="repeat(3,1fr)" gap={2}>
+                                    {themeProps.map((x: string)=>(
+                                        <div key={x}>
+                                            <Text fontSize="10">{x}</Text>
+                                            <Card size="lg">
+                                                <CardBody></CardBody>
+                                            </Card>
+                                        </div>
+                                    ))}
+                                </Grid>
+                            </CardBody>
+                        </Card>
                     </div>
-                </div>
-            </Grid>
+                    <div>
+                        <b>Preview</b>
+                        <Image boxSize="lg" src="https://via.placeholder.com/500x300/0077be/ffffff?text=Placeholder+Image"></Image>
+                        <Button colorScheme="blue">Download .lua File</Button>
+                    </div>
+                    <div>
+                        <Button colorScheme="blue">
+                            <Link to="edit">Edit Presets</Link>
+                        </Button>
+                        <Accordion>
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton>
+                                        <Box as="span" flex='1' textAlign='left'>
+                                      Presets
+                                        </Box>
+                                        <AccordionIcon />
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                              commodo consequat.
+                                </AccordionPanel>
+                            </AccordionItem>
+
+                            <AccordionItem>
+                                <h2>
+                                    <AccordionButton>
+                                        <Box as="span" flex='1' textAlign='left'>
+                                      Saved Themes
+                                        </Box>
+                                        <AccordionIcon />
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                              commodo consequat.
+                                </AccordionPanel>
+                            </AccordionItem>
+                        </Accordion>
+                        <div style={{bottom: 0, position: "absolute"}}>
+                            <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                                <Select placeholder='Select option'>
+                                    <option value='option1'>Option 1</option>
+                                    <option value='option2'>Option 2</option>
+                                    <option value='option3'>Option 3</option>
+                                </Select>
+                                <Button colorScheme="blue" >
+                                    <Link to="users">Edit Users</Link>
+                                </Button>
+                            </Grid>
+                        </div>
+                    </div>
+                </Grid>
+            </div>
         </ChakraProvider>
     );
 }
