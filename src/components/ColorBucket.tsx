32755a8d9ce3d634a74c466932274d7d33b8af49
johnma <ma.jonathan02@gmail.com>
2023-04-15 17:47:51 -0400

    added themeFile logic to ColorBuckets

--- src/components/ColorBucket.tsx
+++ src/components/ColorBucket.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { BackgroundProps, Card, CardBody } from "@chakra-ui/react";
 import { useDrop } from "react-dnd";
+import { ThemeContext } from "@emotion/react";
 
 interface droppedItem{
     fillColor: string
 }
 
-export default function ColorBucket() {
-    const [fillColor, setFillColor] = useState<string>("white");
+interface themeFile{
+    [name: string]: string;
+}
+interface BucketProps {
+    theme: themeFile;
+    setThemeFile: (x: themeFile) => void;
+    prop: string
+}
+
+export default function ColorBucket({theme, setThemeFile, prop}: BucketProps) {
+    const [fillColor, setFillColor] = useState<string>("#ffffff");
     const [collectedProps, dropRef] = useDrop(() => ({
         accept: "CARD",
         drop: (item, monitor) =>{
             const dropped = item as droppedItem;
             setFillColor(dropped.fillColor);
+            const newTheme = { ...theme, [prop]: dropped.fillColor };
+            setThemeFile(newTheme);
         }
     }));
     return (
