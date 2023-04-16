8e5eacddc51d2575f2ef292f060be26bc19a7407
craftablescience <me@craftablescience.info>
2023-04-16 00:09:40 -0400

    chore: remove unnecessary <></> tag and set color picker to black by default (contrasts with bg better)

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -4,19 +4,17 @@ import DragColor from "./DragColor";
 import {ChromePicker} from "react-color";
 
 export default function ColorPicker() {
-    const [color, setColor] = useState<string>("#FFFFFF");
+    const [color, setColor] = useState<string>("#000000");
     return (
-        <>
-            <Grid templateColumns="repeat(2,1fr)" alignItems="center">
-                <GridItem w="25vh" paddingLeft="12">
-                    <DragColor fillColor={color}></DragColor>
-                </GridItem>
-                <ChromePicker
-                    color={color}
-                    disableAlpha={true}
-                    onChangeComplete={(color)=>setColor(color.hex)}
-                />
-            </Grid>
-        </>
+        <Grid templateColumns="repeat(2,1fr)" alignItems="center">
+            <GridItem w="25vh" paddingLeft="12">
+                <DragColor fillColor={color}></DragColor>
+            </GridItem>
+            <ChromePicker
+                color={color}
+                disableAlpha={true}
+                onChangeComplete={(color)=>setColor(color.hex)}
+            />
+        </Grid>
     );
 }
