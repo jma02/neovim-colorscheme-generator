7cb96cbcf94d725e09a10f205531532f3669c445
Eli Brignac <ebrignac@udel.edu>
2023-05-06 19:25:15 -0400

    ColorBucket, ColorPicker, DragColor Tests

---
+++ src/Tests/ColorPicker.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+//@ts-ignore
+import DragColor from "./DragColor";
+import { ChromePicker } from "react-color";
+
+describe("DragColor component", () => {
+    it("should update the color when the ChromePicker value changes", () => {
+        const { getByTestId } = render(
+            <>
+                <DragColor />
+                <ChromePicker data-testid="color-picker" />
+            </>
+        );
+        const dragColor = getByTestId("drag-color");
+        const colorPicker = getByTestId("color-picker");
+
+        fireEvent.change(colorPicker, { target: { color: { r: 255, g: 0, b: 0 } } });
+
+        expect(dragColor).toHaveStyle("background-color: rgb(255, 0, 0)");
+    });
+});
