efadf13e07488eecd27e93d09481206f30396be4
Eli Brignac <ebrignac@udel.edu>
2023-05-06 20:29:40 -0400

    Place Holder for Editor.tsx tests

---
+++ src/Tests/Editor.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+//@ts-ignore
+import PresetPostButton from "./PresetPostButton";
+//@ts-ignore
+import { post_preset } from "./post_preset";
+
+describe("PresetPostButton", () => {
+    it("calls the post_preset function when clicked", () => {
+        //Place holder
+    });
+});
