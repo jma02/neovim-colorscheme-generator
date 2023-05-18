ec5c0580239d0f286b930a57e27ef714b483ed96
Eli Brignac <ebrignac@udel.edu>
2023-05-17 20:58:32 -0400

    test: Added 6 tests to the ColorPicker

--- src/Tests/ColorPicker.test.tsx
+++ src/Tests/ColorPicker.test.tsx
@@ -1,8 +1,71 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import React, { ReactElement, ReactNode } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { HashRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import MatchMediaMock from "jest-matchmedia-mock";
+import ColorPicker from "../components/ColorPicker";
+import { chakraTheme } from "../chakraTheme";
+import DragColor from "../components/DragColor";
+import { ChromePicker } from "react-color";
 
-describe("ColorPicker", () => {
-    it("calls the post_preset function when clicked", () => {
-        //Place holder
+new MatchMediaMock();
+
+
+export const renderColorPicker = () => {
+    const t = extendTheme(chakraTheme);
+    const colorBuck = render(
+        <React.StrictMode>
+            <ChakraProvider theme={t}>
+                <DndProvider backend={HTML5Backend}>
+                    <HashRouter>
+                        <ColorPicker/>
+                    </HashRouter>
+                </DndProvider>
+            </ChakraProvider>
+        </React.StrictMode>
+    );
+    return colorBuck;
+};
+
+
+describe("ColorPicker tests", () => {
+
+    test("renders color picker", () => {
+        const cp = renderColorPicker();
+        expect(cp);
     });
+
+    test("Checks for color wheel", () => {
+        const cp = renderColorPicker();
+        expect(<ChromePicker
+            color={"#000000"} />).toBeInTheDocument;
+    }
+    );
+
+    test("checks color in the wheel", () => {
+        const cp = renderColorPicker();
+        expect("#000000").toBeInTheDocument;
+    }
+    );
+
+    test("checks DragColor opject", () => {
+        const cp = renderColorPicker();
+        expect(<DragColor fillColor={"#000000"}/>).toBeInTheDocument;
+    }
+    );
+
+    test("Checks for Hex", () => {
+        const cp = renderColorPicker();
+        expect("Hex").toBeInTheDocument;
+    }
+    );
+
+    test("Checks for RGB", () => {
+        const cp = renderColorPicker();
+        expect("RGB").toBeInTheDocument;
+    }
+    );
+
 });
